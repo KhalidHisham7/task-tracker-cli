18e9132c09b2ad7d7b2c6c28c5b0dcdf596cfc09
Timon <timoncr9@gmail.com>
2025-09-03 02:49:46 +0300

    Created add functionality

--- task-tracker-cli.py
+++ task-tracker-cli.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 import os
 import json
 from datetime import datetime
-
-id_incremental = 0
+import sys
 
 def main():
-    # Setup main runtime and get operation argument
-    parser = argparse.ArgumentParser(description="Hello, this is a task tracker CLI.")
-    parser.add_argument("operation")
-    args = parser.parse_args()
-    operation = args.operation
-
     # open file if it exists, otherwise create
     file_name = 'tasks.json'
     tasks_json = {}
@@ -23,14 +15,34 @@ def main():
     read_file = open(file_name, 'r+')
     tasks_json = json.loads(read_file.read())
 
+    operation = sys.argv[1]
+    match operation:
+        case 'add':
+            if len(sys.argv) > 2:
+                tasks_json['tasks'].append({
+                    'id': len(tasks_json['tasks']) + 1,
+                    'description': sys.argv[2],
+                    'status': 'todo',
+                    'created_at': str(datetime.now()),
+                    'updated_at': str(None)
+                })
+            else:
+                print("Description is required!\n")
+        case 'update':
+            print("update")
+        case 'delete':
+            print("update")
+        case 'mark-in-progress':
+            print("mark-in-progress")
+        case 'mark-done':
+            print("mark-done")
+        case 'list':
+            print("list")
+
+
     write_file = open(file_name, 'w')
     json.dump(tasks_json, write_file, sort_keys=True, indent=4)
-    
 
-def incrementing_id():
-    global id_incremental
-    id_incremental += 1
-    
 
 if __name__ == "__main__":
     main()
